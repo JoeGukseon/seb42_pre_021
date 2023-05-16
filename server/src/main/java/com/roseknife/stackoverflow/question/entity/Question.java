0b8bb225be04644d5f1637826dc53e683b600732
JoeGukseon <zipcks1381@gmail.com>
2023-05-17 00:37:43 +0900

    :recycle: refactor : Question Entity Setter 제거 / Builder 추가

--- server/src/main/java/com/roseknife/stackoverflow/question/entity/Question.java
+++ server/src/main/java/com/roseknife/stackoverflow/question/entity/Question.java
@@ -7,18 +7,18 @@ import com.roseknife.stackoverflow.comment.entity.QuestionComment;
 import com.roseknife.stackoverflow.bookmark.entity.QuestionBookmark;
 import com.roseknife.stackoverflow.member.entity.Member;
 import com.roseknife.stackoverflow.tag.entity.QuestionTag;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-@NoArgsConstructor
+//Setter 제거 @Builder, @AllArgsConstructor 추가
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Setter
+@Builder
 @Entity
 public class Question extends Auditable {
     @Id
@@ -35,9 +35,32 @@ public class Question extends Auditable {
     private String markdown;
 
     //카운트 초기화 엔티티에서 하는게 유리 - 23.5.16 리팩토링
+    /*
+        컬렉션은 필드에서 초기화 하자.
+        컬렉션은 필드에서 바로 초기화 하는 것이 안전하다.
+        null 문제에서 안전하다.
+        빌더 초기화 로 @Builder.dafault
+     */
+    @Builder.Default
     private Integer viewCount=0;
+    @Builder.Default
     private Integer answerCount=0;
+    @Builder.Default
     private Integer voteCount=0;
+
+    public void viewCountPlus() {
+        viewCount++;
+    }
+    public void answerCountPlus() {
+        answerCount++;
+    }
+    public void answerCountMinus() {
+        if(answerCount > 0){ answerCount--;}
+    }
+    public void voteCountPlus() {
+        voteCount++;
+    }
+
 
 //    @JsonIgnore
     @ManyToOne
@@ -45,6 +68,7 @@ public class Question extends Auditable {
     private Member member;
 
     @JsonIgnore //목록으로 가져오는 쪽 에서만 적용해도 가능
+    @Builder.Default
     @OneToMany(mappedBy = "question",cascade = CascadeType.REMOVE)    //
     private List<Answer> answers = new ArrayList<>();
 
@@ -52,9 +76,22 @@ public class Question extends Auditable {
     @OneToMany(mappedBy = "question",cascade = CascadeType.REMOVE)
     private List<QuestionComment> questionComments = new ArrayList<>();
 
+    //질문글과 같이 questionTag의 값이 같이 움직이기 때문에 양방향 맵핑 해야함!(글 생성 및 삭제)
     @JsonIgnore
     @OneToMany(mappedBy = "question", cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
     private List<QuestionTag> questionTags = new ArrayList<>();
+
+    //진짜 맵핑을 하는 QuestionTag가 키를 가지고 있기 때문에 이쪽에서 값을 업데이트 할필요가 x
+//    public void setQuestionTags(List<QuestionTag> questionTags) {
+//        this.questionTags = questionTags;
+//    }
+//
+//    public void addQuestionTag(QuestionTag questionTag) {
+//        this.questionTags.add(questionTag);
+//        if (questionTag.getQuestion() != this) {
+//            questionTag.setQuestion(this);
+//        }
+//    }
 
 //    @OneToMany(mappedBy = "question")
 //    private List<QuestionVote> questionVotes; //voteCount 로 대체
