eac1a9678dda8911ed84d712955d54eb83e8ef96
JoeGukseon <zipcks1381@gmail.com>
2023-07-05 11:56:59 +0900

    :recycle: refactor : 조회에서 N+1 수정 fetch join 테스트 적용

--- server/src/main/java/com/roseknife/stackoverflow/question/repository/QuestionRepository.java
+++ server/src/main/java/com/roseknife/stackoverflow/question/repository/QuestionRepository.java
@@ -1,8 +1,10 @@
 package com.roseknife.stackoverflow.question.repository;
 
 import com.roseknife.stackoverflow.question.entity.Question;
+import org.hibernate.annotations.BatchSize;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,7 +15,38 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
 
 //    @Query(value = "select * from question where (title || content) like %:keyword%",nativeQuery = true)
 //    List<Question> searchQuestionsByKeyword(@Param("keyword") String keyword);
+//@EntityGraph(value = "Question.withAssociations", type = EntityGraph.EntityGraphType.FETCH)
+
+    //EntityGraph로 fetch join 적용
+    @EntityGraph(attributePaths = {"member","questionBookmark"})
     Page<Question> findByTitleContainsOrHtmlContains(String titleKeyword,String htmlKeyword,Pageable pageable);
 
+    //fetch join JPQL로 적용
+    @Query(value = "SELECT q FROM Question q " +
+            "JOIN FETCH q.member m " +
+//            "LEFT JOIN FETCH q.questionTags qt " +
+//            "LEFT JOIN FETCH m.roles "+
+            "LEFT JOIN FETCH q.questionBookmark ",
+            countQuery = "SELECT COUNT(q) FROM Question q")
+    Page<Question> findQuestionsWithAssociations(Pageable pageable);
 
+//    @Query(value = "SELECT q FROM Question q " +
+//            "JOIN FETCH q.member m " +
+////            "LEFT JOIN FETCH q.questionTags qt " +
+////            "LEFT JOIN FETCH qt.tag " +
+//            "LEFT JOIN FETCH q.member.roles " ,
+////            "LEFT JOIN FETCH q.questionBookmark ",
+//            countQuery = "SELECT COUNT(q) FROM Question q")
+//    Page<Question> findQuestionsWithAssociations(Pageable pageable);
+
+//    @Query(value = "SELECT q FROM Question q " +
+//        "JOIN FETCH q.member m " +
+//        "LEFT JOIN FETCH m.roles " ,
+//        countQuery = "SELECT COUNT(q) FROM Question q")
+//    Page<Question> findQuestionsWithAssociations(Pageable pageable);
+
+
+//    @EntityGraph(attributePaths = {"member","questionBookmark","questionTags"})
+//    @Query("SELECT q FROM Question q")
+//    Page<Question> findQuestionsWithAssociations(Pageable pageable);
 }
